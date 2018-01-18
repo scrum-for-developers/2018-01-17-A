f30c346a48fbbbddf7df2d63111e7355f9a49b8a
Anna Backs <anna.backs@codecentric.de>
2018-01-18 16:03:53 +0100

    WB-112 WIP

--- worblehat-web/src/main/java/de/codecentric/psd/worblehat/web/controller/ShowAllBooksByBorrowerController.java
+++ worblehat-web/src/main/java/de/codecentric/psd/worblehat/web/controller/ShowAllBooksByBorrowerController.java
@@ -1,9 +1,9 @@
 package de.codecentric.psd.worblehat.web.controller;
 
-import de.codecentric.psd.worblehat.domain.Book;
-import de.codecentric.psd.worblehat.domain.BookService;
-import de.codecentric.psd.worblehat.web.formdata.ReturnAllBooksFormData;
-import de.codecentric.psd.worblehat.web.formdata.ShowAllBooksByBorrowerFormData;
+import java.util.List;
+
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
@@ -12,37 +12,38 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-import javax.validation.Valid;
-import java.util.List;
+import de.codecentric.psd.worblehat.domain.BookService;
+import de.codecentric.psd.worblehat.domain.Borrowing;
+import de.codecentric.psd.worblehat.web.formdata.ShowAllBooksByBorrowerFormData;
 
 @Controller
 @RequestMapping("/showAllBooksByBorrower")
 public class ShowAllBooksByBorrowerController {
 
-    private BookService bookService;
+	private BookService bookService;
 
-    @Autowired
-    public ShowAllBooksByBorrowerController(BookService bookService) {
-        this.bookService = bookService;
-    }
+	@Autowired
+	public ShowAllBooksByBorrowerController(BookService bookService) {
+		this.bookService = bookService;
+	}
 
-    @RequestMapping(method = RequestMethod.GET)
-    public void prepareView(ModelMap modelMap) {
-        modelMap.put("showAllBooksByBorrowerFormData", new ShowAllBooksByBorrowerFormData());
-    }
+	@RequestMapping(method = RequestMethod.GET)
+	public void prepareView(ModelMap modelMap) {
+		modelMap.put("showAllBooksByBorrowerFormData", new ShowAllBooksByBorrowerFormData());
+	}
 
-    @RequestMapping(method = RequestMethod.POST)
-    public String showAllBooksByBorrower(
-            @ModelAttribute("showAllBooksByBorrowerFormData") @Valid
-                    ShowAllBooksByBorrowerFormData formData,
-            BindingResult result, ModelMap modelMap) {
-        if (result.hasErrors()) {
-            return "showAllBooksByBorrower";
-        } else {
-            List<Book> allBooksByBorrower = bookService.getAllBooksByBorrower(formData.getEmailAddress());
-            modelMap.put("books", allBooksByBorrower);
+	@RequestMapping(method = RequestMethod.POST)
+	public String showAllBooksByBorrower(
+			@ModelAttribute("showAllBooksByBorrowerFormData") @Valid
+			ShowAllBooksByBorrowerFormData formData,
+			BindingResult result, ModelMap modelMap) {
+		if (result.hasErrors()) {
+			return "showAllBooksByBorrower";
+		} else {
+			List<Borrowing> allBorrowingByBorrower = bookService.getAllBorrowingByBorrower(formData.getEmailAddress());
+			modelMap.put("borrowings", allBorrowingByBorrower);
 
-            return "bookList";
-        }
-    }
+			return "borrowedBooksList";
+		}
+	}
 }
